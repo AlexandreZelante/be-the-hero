911b593f13e1a17f3d77c4d9a10ab79da887d70b
Alexandre Zelante <alexandre.zelante@outlook.com>
2020-03-28 21:14:57 -0300

    styling ok

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FiPower } from "react-icons/fi";
+import { FiPower, FiTrash2 } from "react-icons/fi";
 
 // import { Container } from './styles';
 import logoImg from "../../assets/logo.svg";
+import "./styles.css";
 
 export default function Profile() {
   return (
@@ -19,6 +20,25 @@ export default function Profile() {
           <FiPower size={18} color="#E02041" />
         </button>
       </header>
+
+      <h1> Casos Cadastrados</h1>
+
+      <ul>
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
